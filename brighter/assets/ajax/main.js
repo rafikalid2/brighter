b305eb72d10833863e87b92d692c6bbf12b44e9d
rafikalid <khalid.rfk@gmail.com>
2017-10-10 22:48:06 +0100

    cc

---
+++ brighter/assets/ajax/main.js
@@ -0,0 +1,24 @@
+/**
+ * pourvoir grouper les appel ajax
+ * se rappler de tous les appel ajax, voir par groupe
+ *
+ * $$.get(baseUrl, GetData)
+ * $$.post(baseURL, GetData, PostData)
+ *
+ *
+ * 		.then((data, err)=>{})
+ * 		.downloadProgress(fx)	// get progress or add listener to onuploadprogress
+ * 		.uploadProgress(fx)		// get progress or add listener to onuploadprogress
+ * 		.status(fx) 			// get status, add fx to status change
+ *
+ * 		.request
+ * 			.header // get all header, get a header, set all headers, set a header
+ * 			.dataType
+ * 			.upoadProgress
+ * 			.and. // go back to root object
+ * 		.response
+ * 			.dataType
+ * 			.header
+ * 			.downloadProgress
+ * 			.and. // go back to root object
+ */
--- brighter/assets/arrays/mains.js
+++ brighter/assets/arrays/mains.js
@@ -1,6 +1,89 @@
-$$.array(array)
+/**
+ * Arrays utils
+ */
+	var $$Arrays = ArrayUtils.prototype;
+	/**
+	 * create new Array or transforme an existing one
+	 */
+		$$.array	= ArrayUtils;
+	 	function ArrayUtils(array){
+	 		if(!array)
+				array	= [];
+			array.__proto__	= $$Arrays;
+	 	}
 
-$$Arrays // the prototype
+ 	classExtend(Array, ArrayUtils);// make array utils as a subclass
+
+	Object.defineProperties($$Arrays, {
+		// dupplicate array
+			duplicate	: {
+				value : function(){
+					return this.slice(0);
+				}
+			},
+		// find first occurrence that matches a condition, or undefined
+			first		: {
+				value: function(condition){
+					for(var i=0, c = this.length; i < c; ++i)
+						if(condition.call(this[i], this[i], i))
+							return this[i];
+				}
+			},
+		// find the last element that matches a condition, or undefined
+			last		: {
+				value: function(condition){
+					for(var i = this.length - 1 ; i >= 0; --i)
+						if(condition.call(this[i], this[i], i))
+							return this[i];
+				}
+			},
+		// contains
+			has	: {
+				value	: function(){
+					for(var i = 0, c = arguments.length ; i < c; ++i){
+						if(this.indexOf(arguments[i]) == -1)
+							return false;
+					}
+					return true;
+				}
+			},
+		// forEach
+			forEach	: _arrayUtilEach,
+			each	: _arrayUtilEach
+	});
+
+
+// wrappers
+	['push', 'unshift', 'reverse', 'sort']
+		.each(function(ele){
+			Object.defineProperty($$Arrays[ele], ele, {
+				value	: function(){
+					Array.prototype[ele].apply(this, arguments);
+					return this;
+				}
+			});
+		});
+// wrappers 2
+	['concat', 'slice', 'splice', 'filter', 'map']
+		.each(function(ele){
+			Object.defineProperty($$Arrays[ele], ele, {
+				value	: function(){
+					var lst			= Array.prototype[ele].apply(this, arguments);
+					lst.__proto__	= $$Arrays;
+					return lst;
+				}
+			});
+		});
+// forEach
+// break when callBack returns false
+	function _arrayUtilEach(callBack){
+		for(var i=0, c = this.length; i < c; ++i){
+			if(callBack.call(this[i], this[i], i) === false)
+				break;
+		}
+	}
+	
+
 
 //
 //contains a set of utils for arrays
