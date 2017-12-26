001af363a3766c14afa0c15e126dd32b8d637fa5
rafikalid <khalid.rfk@gmail.com>
2017-12-26 15:01:52 +0000

    cc

--- back/doc/brighter-js/ajax.js
+++ back/doc/brighter-js/ajax.js
@@ -13,39 +13,36 @@
 **/
 // ajax commons	
 	$$.ajax
-		.xhrs				// list of current connections
-
-		.abort()				// abort all current connections
-		.abort('a.b b.c')		// abort those groups
-		.abort(xhr => {})		// when a connection is aborted
-
-		.catch((err, xhr) => {})	// add this callback to when error
-
-		.status({
-			code : xhr => {}
-		}) // execute when this code
-
-		.bind('event', listener)
-		.unbind()
-
-		// .urlDecoder(url => url) // optional, decoder to decode URL
-		.urlDecoder(/pattern/, decoder)
-
-		.find('a.b c')	// get all xhrs from those groups
-			.status({})
-			.catch()
-			.abort()
-
-		.plugin('fx', value);// add a plugin to ajax
-
-
-		/**
-		 * @param {regex} urlPattern match an url already in progress or in the queu, default to undefined (no waiting, exec request immediately)
-		 * @param {int} timeout time to wait after the previous url in queu
-		 * @param {int} queuTimeout max time to wait in the queu, default to 0 (wait intil the previous connection ends)
-		 * @param {boolean} abortWhenQueuTimeout abort the connection if timeout in queu, default to true
-		 */
-		.urlQeu(urlPattern, timeout, queuTimeout, abortWhenQueuTimeout)	// if a request is already in progress, wait this timeout, do not use parallel requests
+		.xhrs						// list of current connections
+		// 
+			.onAdd(xhr => {})		// callBack when a new request is added
+			.onAdd()				// get callBack
+		// abort
+			.abortAll()				// abort all current connections
+			.abort('a.b b.c')		// abort those groups
+			.abort(xhr => {})		// when a connection is aborted
+		// on Error
+			.catch(xhr => {})
+		// timeout
+			timeout()				// get default timeout
+			timeout(int)			// set default timeout
+			timeout(xhr => {})		// when an xhr get in timeout
+		// Done
+			done(xhr => {})			// callBack when a request ends
+		// meta redirect
+			.followMetaRedirects(true|false)	// set default value
+			.followMetaRedirects()				// get default value
+			.onMetaRedirects((url, xhr) => {})	// default "on before following metaredirect", if return is false, == do not follow
+			.onMetaRedirects()					// get default callback
+		// URL
+			.urlDecoder() 			// get default URL decoder
+			.urlDecoder(url => url) // set default URL decoder
+			.urlDecoder([
+				{regex: /^/i, decoder: function(url){}}
+			])
+		// events
+			.bind('event', listener)
+			.unbind()					// unbind all events
 	// events
 		requestAdded	// when a new connection requested
 		requestFinish	// when a request finished
@@ -59,118 +56,141 @@
 	$$.post(url)
 	$$.head(url)
 	$$.delete(url)
-		.group('group1.subGroup group2.subGroup2') // group ajax calls
-		.url // get URL
-		.originalURL	// getter, the ori ginal URL before redirects
-
-		.urlDecoder(url => url) // optional, decoder to decode URL
-		.goToURL(url)	// make redirect to this URL
-
+		// promise functions
+			.then(resp => {}, err => {})
+			.catch(err => {})
+			.finally(() => {})
+		// before send
+			.beforeSend(this => {}) // callBack before start native request
+			.wait(int)				// time to wait in miliseconds before sending this request, default to 0
+			.waitTo(promise)		// wait this promise to ends before starting this call
+		// groupBy
+			.group					// get the current group
+			.groupBy('group1.subGroup group2.subGroup2') // group ajax calls, could be called only once
 		/**
 		 * @param {string} group	 make a group
 		 * @param {int} queuTimeout max time to wait in the queu, default to 0 c (wait intil the previous connection ends)
 		 * @param {boolean} abortWhenQueuTimeout abort the connection if timeout in queu, default to true
 		 * @param {int} timeout time to wait after the previous url in queu
 		 */
-		.lazyRequest({
-			group				: 'groupName',	// group affected by the lazyness
-			wait				: 0, 			// time to wait after previous connection ends, default to 0
-			queuTimeout			: 0, 			// max time to wait in the queu, default to 0 (wait until the )
-			abortWhenTimeout	: true, 		// optional, abort when timeout, otherwise, execute the request, default to true
-			timeout				: 0				// maximum timeout for this connection, this includes queu timeout and XHR timeout
-		})	// if a request is already in progress, wait this timeout, do not use parallel requests
-
-		.cache(false) 				//force de navigator to reload the data from this url, // set or get
-		.timeout(int) 				// timeout, get timeout if the arg is undefined
-		.timeout(xhr=>{}) 			// on timeout
-
-		.followMetaRedirects(true|false)	// if follow meta and script redirections
-		.followMetaRedirects((url, xhr) => {})	// on before following metaredirect, if return is false, == do not follow
-
-
-		.xhr						// the native XHR
-
-		.abort()// abort sending
-		.abort(xhr => {})// when the connection is aborted
-
-		.retry()// retry sending
-		.retry(xhr => {})// when retry
-
-		.param('name')			// get param value
-		.param('name', value)	// add URL param
-		.param({name:value})
-
-		.data(obj || formData || HTMLForm || text) // set or get the data
-
-		.then((data, err)=>{})	// when finish
-
-		.status() 			// get status 200, ...
-		.status({
-			200 	: fx,
-			'40x'	: fx, 	// when the status is 400 - 409
-			'3xx'	: fx,	// when status is 300-399
-			'300-405' : fx // range
-		})
-
-		.readyState()			// get ready state [0, 1, 2, 3, 4]
-		.readyState(fx)			// add this fx as callBack when the state change
-
-
-		.bind('eventName', fx)		// eventName in ['readyState', 'readyStateChange', ]
-		.unbind('eventName')		// unbind all events of type eventName
-		.unbind('eventName', fx)	// inbind the event from
-
-		.request
-		.upload
-			.header() // get all header
-			.header('name')	// get a header
-			.header({key:value}) // set headers
-			.header('key', 'value') // set some header
-
-			.removeHeader('header name', 'header2', '...')// remove header
-
-			.converter(data=>{})// optional, fx that will convert data to text, // set or get
-
-			.progress() // progress 0 to 1
-			.progress((percent, totalBytes, uploadedBytes){})// set or get, valable aussi on top object
-			.progress($$progressBar)// adjust progressbar auto
-
-			.contentType()// get content type
-			.contentType('type')// override content type [abbrv like: json, html, text, ...] or complete mimetype
-
-			.charset()			// get charset
-			.charset(charset)	// override charset [utf-8, ...]
-
+			.isLazy								// is the request in the lazy
+			.lazy('groupName')
+			.lazyTimeout(int)					// timeout to wait in the lazy mode
+			.lazyTimeout(xhr => true | false)	//when lazy timeout, does proceed with the request or abort it
+		// URL
+			.urlDecoder(url => url) // optional, decoder to decode URL
+			.urlDecoder([
+				{regex: /^/i, decoder: function(url){}}
+			])
+			.param()				// get all GET params as object
+			.param('name')			// get GET param value
+			.param('name', value)	// set URL GET param
+			.param({name:value, att2 : ['value1', 'value2']})
+		// data
+			.data(obj || formData || HTMLForm || HTMLInput || text) // set or get the data
+			.dataType()				// get the dataType
+			.dataType(mimetype)		// set the data type
+			.dataCharset()			// get data charset
+			.dataCharset(charset)	// set data charset
+		// response
+			.accepts()					// get accepted data request as array
 			.accepts('json', '', ,,,)	// accepted server responses, valable aussi on top object
+		// headers
+			.header()				// get all headers as object
+			.header('headerName')	// get headerName
+			.header('headerName', str || array[str]) // set header value or values
+			.removeAllHeaders()			// remove all headers
+			.removeHeader('headerName')	// remove headerName
+			.removeHeader(['headerName1', 'headerName2', ...])	// remove those headers
+			.removeHeader('headerName', 'headerValue' | ['headerValues'])	// remove headerName that has this value
+			.removeHeader({
+				header1: value1,
+				header2: [v1, v2, ...]
+			})	// remove headerName
 
-			.beforeSend(xhr => {})// get or set before send callBack
-		.response
-		.download
-			.progress() // progress 0 to 1
-			.progress((percent, totalBytes, downloadedBytes)=>{})// set or get, valbale aussi on top object
-			.progress($$progressBar)// adjust progressbar auto
+			.responseHeaders		// get all response headers
+		// progress
+			.uploadProgress()		// get the upload progress
+			.uploadProgress(cb)		// add listener to on upload progress
+			.downloadProgress()		// get the download progress
+			.downloadProgress(cb)	// add listener to on donwload progress
+		// redirects
+			.redirectTo(url)	// make redirect to this URL, throws error if request ends
+			.followMetaRedirects(true|false)	// if follow meta and script redirections, default to false
+			.onMetaRedirects((url, xhr) => {})	// on before following metaredirect, if return is false, == do not follow
+		// GETTERS
+			.url 			// get URL
+			.originalURL	// getter, the ori ginal URL before redirects
+			.xhr			// get the native XHR
+		// options
+			.cache(false) 		//force de navigator to reload the data from this url, // set or get
+		// timeout
+			.timeout()			// get global timeout
+			.timeout(int) 		// set global timeout
+			.timeout(err => {}) // on timeout (global, queu or xhr timeout)
 
+			.xhrTimeout()		// get xhr timeout
+			.xhrTimeout(int)	// set timeout to XHR
+		// state
+			.abort()// abort sending
+			.abort(xhr => {})// when the connection is aborted
 
-			.loaded(data => data)	// when the data is loaded
-			.converter(data => data)	// get or set converter
-
-			.header()		// get all headers
-			.header('name')	// get a header
-
-			.header(xhr=>{})// call when headers received (state = 2s)
-
-			.contentType()// get content type	
+			.reply()	// reply request, returns new request
+		// status
+			.status() 			// get status 200, ...
+			.status({
+				200 		: (resp, xhr) => {},
+				'40x'		: (resp, xhr) => {}, 	// when the status is 400 - 409
+				'3xx'		: (resp, xhr) => {},	// when status is 300-399
+				'300-405'	: (resp, xhr) => {} // range
+			})
+		// ready state
+			.state						// current state: [lazy, unsent, opened, headerReceived, timeout, xhrTimeout, lazyTimeout]
+			.readyState()				// get ready state [-1: lazyState, 0, 1, 2, 3, 4]
+			.readyState(fx)				// add this fx as callBack when the state change
 
 		// events
-			load
-			progress
-			error
-			abort
+		/**
+		 * 0 : xhrUnsent
+		 * 1 : xhrOpened
+		 * 2 : xhrHeadersReceived
+		 * 3 : xhrLoading
+		 * 4 : xhrDone
+		 *
+		 * metaRedirect
+		 * 
+		 * uploadProgress 
+		 * downloadProgress 
+		 * error 
+		 * abort
+ 
+		 * timeout
+		 * readyStateChange
+		 * 
+		 * done
+		 */
+			.on('eventName', fx)		// eventName in ['readyState', 'readyStateChange', ]
+			.off('eventName')			// unbind all events of type eventName
+			.off('eventName', fx)		// inbind the event from
+		// serializer/ deseralizer
+			.serializer()				// get the used serializer
+			.serializer(data => data)	// set the serializer for this request
+			.serializer({				// add serializer manager for this request
+				dataMimeType : (data => data)
+			})
 
-			timeout
-			readyStateChange
+			.deserializer()				// get the used deserializer
+			.deserializer(data => data)	// set the used deserializer
+			.deserializer({				// add deserializer manager for this request
+				responseMimeType : (data => data)
+			})
 
-			loadend // when the tree events: load, error and abort
+
+
+			
+
+		// events
+			
 
 
 // on elements
---
+++ brighter/assets/core/ajax/ajax.js
@@ -0,0 +1,21 @@
+
+
+$$.plugin(true, {
+	ajax	: {
+
+	}
+});
+
+(function(){
+	var _ajax	= {};
+	// add methodes
+	Object.defineProperties(_ajax, {
+		
+	});
+
+
+	// add interface
+	$$.plugin(true, {
+		ajax	: _ajax
+	});
+})();
